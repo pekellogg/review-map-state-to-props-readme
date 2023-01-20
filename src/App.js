746be5433532296815645fe169692858f1087869
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-19 16:32:01 -0800

    commit tree efb8fe36473724c3f3aa0e69fb1de4c0e9c02cd9

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
 
 
 class App extends Component {
 
   handleOnClickItems = () => {
     this.props.dispatch({
-      type: 'GET_COUNT_OF_ITEMS',
+      type: "GET_COUNT_OF_ITEMS",
     });
   }
 
   handleOnClickUsers = () => {
     this.props.dispatch({
-      type: 'GET_COUNT_OF_USERS',
+      type: "GET_COUNT_OF_USERS",
     });
   }
 
@@ -34,7 +34,6 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
   return { items: state.items }
 }
 
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,21 +1,19 @@
 export default function shoppingListItemReducer(state = {
   items: [], 
-  users: [ 'initial user' ]
+  users: [ "initial user" ]
 }, action) {
 
   switch(action.type) {
-
-    case 'GET_COUNT_OF_ITEMS':
+    case "GET_COUNT_OF_ITEMS":
       return Object.assign({}, state, {
         items: state.items.concat(state.items.length + 1)
       });
-
-    case 'GET_COUNT_OF_USERS':
+    case "GET_COUNT_OF_USERS":
       return Object.assign({}, state, {
         users: state.users.concat(state.users.length + 1)
       });
-
     default:
       return state;
-  }
+  };
+
 };
